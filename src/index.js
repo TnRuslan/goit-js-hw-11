1f58e9cc283f10cb9a4a5947b19d985eaa4a73a7
Ruslan Tendora <ruslantendora@gmail.com>
2022-12-04 18:49:52 +0200

    add libraries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import './css/styles.css';
+const axios = require('axios').default;
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const DEBOUNCE_DELAY = 300;
+const form = document.querySelector('#search-form');
+
+axios({
+  method: 'get',
+  url: 'https://pixabay.com/api/',
+  params: {
+    key: '31826070-5147e993357879bd9f8310722',
+    q: 'cat',
+    image_tipe: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  },
+}).then(r => console.log(r.data.hits));
